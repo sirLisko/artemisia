01e110dcd1c95868f32c3c0334c4eccb88ba2523
Luca Lischetti <luca@sirlisko.com>
2018-12-28 22:33:31 +0100

    improved about and story

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
+import { graphql, Link } from 'gatsby';
+import styled from '@emotion/styled';
+
+import Layout from 'src/components/Layout';
+import theme from 'src/theme';
+
+const GridColumn = styled.div`
+  & + & {
+    margin-top: 3rem;
+  }
+  img {
+    margin-top: 2rem;
+  }
+  ${theme.mq.medium} {
+    display: grid;
+    grid-template-columns: minmax(min-content, ${theme.sizes.column}) minmax(
+        min-content,
+        ${theme.sizes.column}
+      );
+    grid-gap: 5rem;
+    align-items: center;
+    justify-content: center;
+    img {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+    &:nth-of-type(odd) {
+      img {
+        order: -1;
+      }
+    }
+  }
+
+  a {
+    color: ${theme.colors.black};
+    background: #fff;
+    border: 2px solid currentColor;
+    padding: 0.75rem 0;
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 0.14em;
+    display: block;
+    width: 160px;
+    margin: 2rem auto;
+    font-size: 14px;
+    transition: all 0.4s ease-in-out;
+    text-align: center;
+    &:hover {
+      color: #fff;
+      background: ${theme.colors.black};
+    }
+`;
 
 const Index = ({ data }) => {
   const { edges } = data.allPerson;
   return (
     <Layout>
       {edges.map(edge => {
-        const { name, overview } = edge.node;
+        const { name, overview, image } = edge.node;
         return (
-          <div key={name} style={{ marginBottom: '1rem' }}>
-            <h3>{name}</h3>
+          <GridColumn key={name}>
             <div>
-              {overview.map((text, i) => (
-                <p key={i}>{text.children[0].text}</p>
-              ))}
+              <h2>{name}</h2>
+              <div>
+                {overview.map((text, i) => (
+                  <p key={i}>{text.children[0].text}</p>
+                ))}
+              </div>
             </div>
-          </div>
+            {image && image.imageUrl && <img src={image.imageUrl} alt="" />}
+          </GridColumn>
         );
       })}
     </Layout>
@@ -30,16 +83,18 @@ Index.propTypes = {
 
 export const query = graphql`
   query {
-    allPerson {
+    allPerson(sort: { fields: [name], order: ASC }) {
       edges {
         node {
-          id
           name
           overview {
             style
             children {
               text
             }
+          }
+          image {
+            imageUrl
           }
         }
       }
--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -1,7 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql, Link } from 'gatsby';
+import styled from '@emotion/styled';
+
 import Layout from 'src/components/Layout';
+import theme from 'src/theme';
+
+const GridColumn = styled.div`
+  & + & {
+    margin-top: 3rem;
+  }
+  img {
+    margin-top: 2rem;
+  }
+  ${theme.mq.medium} {
+    display: grid;
+    grid-template-columns: minmax(min-content, ${theme.sizes.column}) minmax(
+        min-content,
+        ${theme.sizes.column}
+      );
+    grid-gap: 5rem;
+    align-items: center;
+    justify-content: center;
+    img {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+    &:nth-of-type(odd) {
+      img {
+        order: -1;
+      }
+    }
+  }
+
+  a {
+    color: ${theme.colors.black};
+    background: #fff;
+    border: 2px solid currentColor;
+    padding: 0.75rem 0;
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 0.14em;
+    display: block;
+    width: 160px;
+    margin: 2rem auto;
+    font-size: 14px;
+    transition: all 0.4s ease-in-out;
+    text-align: center;
+    &:hover {
+      color: #fff;
+      background: ${theme.colors.black};
+    }
+`;
 
 const Index = ({ data }) => {
   const { edges } = data.allStory;
@@ -9,18 +59,19 @@ const Index = ({ data }) => {
     <Layout>
       {edges.map(edge => {
         const { title, overview, image, link_title, link_url } = edge.node;
-        console.log(image);
         return (
-          <div key={title} style={{ marginBottom: '1rem' }}>
-            <h3>{title}</h3>
-            {image && image.imageUrl && <img src={image.imageUrl} alt="" />}
+          <GridColumn key={title}>
             <div>
-              {overview.map((text, i) => (
-                <p key={i}>{text.children[0].text}</p>
-              ))}
+              <h2>{title}</h2>
+              <div>
+                {overview.map((text, i) => (
+                  <p key={i}>{text.children[0].text}</p>
+                ))}
+              </div>
+              {link_title && <Link to={link_url}>{link_title}</Link>}
             </div>
-            {link_title && <Link to={link_url}>{link_title}</Link>}
-          </div>
+            {image && image.imageUrl && <img src={image.imageUrl} alt="" />}
+          </GridColumn>
         );
       })}
     </Layout>
@@ -33,7 +84,7 @@ Index.propTypes = {
 
 export const query = graphql`
   query {
-    allStory {
+    allStory(sort: { fields: [order], order: ASC }) {
       edges {
         node {
           title
@@ -43,6 +94,12 @@ export const query = graphql`
               text
             }
           }
+          link_title
+          link_url
+          image {
+            imageUrl
+          }
+          order
         }
       }
     }
