a51148c3fc47ab361b7e513c8865d993ecdb04c6
Luca Lischetti <luca@sirlisko.com>
2018-12-29 12:06:51 +0100

    added contact form

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,42 +1,67 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql, Link } from 'gatsby';
-import Layout from 'src/components/Layout';
+import styled from '@emotion/styled';
 
-const Index = ({ data }) => {
+import Layout from 'src/components/Layout';
+import ContactForm from 'src/components/ContactForm';
+import theme from 'src/theme';
+
+const StyledContactForm = styled(ContactForm)`
+  margin-top: 5rem;
+  label {
+    font-weight: 300;
+    font-style: normal;
+  }
+  input,
+  textarea {
+    padding: 12px;
+    margin: 6px 0 4px;
+    border: 1px solid #ccc;
+    background: #fafafa;
+    border-radius: 2px;
+    width: 100%;
+    max-width: 500px;
+  }
+  textarea {
+    max-width: 700px;
+    height: 200px;
+  }
+  button {
+    color: ${theme.colors.black};
+    background: #fff;
+    cursor: pointer;
+    border: 2px solid currentColor;
+    padding: 0.75rem 0;
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 0.14em;
+    display: block;
+    width: 160px;
+    font-size: 14px;
+    transition: all 0.4s ease-in-out;
+    text-align: center;
+    &:hover {
+      color: #fff;
+      background: ${theme.colors.black};
+    }
+  }
+`;
+
+const Index = () => {
   return (
     <Layout>
       <h1>Contact us</h1>
       <p>
         Please complete the form below with all the required information and we
-        will get back to you as soon as possible. At the moment we hold group
-        sessions as well as one to one classes in the comfort of your home, at
-        no additional costs. Prices are available as single classes or packages.
+        will get back to you as soon as possible.
       </p>
+      <p>
+        At the moment we hold group sessions as well as one to one classes in
+        the comfort of your home, at no additional costs.
+      </p>
+      <p>Prices are available as single classes or packages.</p>
+      <StyledContactForm />
     </Layout>
   );
 };
-
-Index.propTypes = {
-  data: PropTypes.shape({}).isRequired,
-};
-
-export const query = graphql`
-  query {
-    allCourse {
-      edges {
-        node {
-          title
-          overview {
-            style
-            children {
-              text
-            }
-          }
-        }
-      }
-    }
-  }
-`;
 
 export default Index;
---
+++ src/pages/thanks.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Layout from 'src/components/Layout';
+
+const Index = () => {
+  return (
+    <Layout>
+      <h1>Thanks</h1>
+      <p>We will be in touch as soon as we can :)</p>
+      <img
+        style={{ marginTop: '5rem' }}
+        src="/imgs/all.png"
+        alt="all together"
+      />
+    </Layout>
+  );
+};
+
+export default Index;
