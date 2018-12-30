ae8f81fbcde99b65c5e26b05e957dc8e824733ed
Luca Lischetti <luca@sirlisko.com>
2018-12-30 11:32:17 +0100

    added analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,11 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA,
+      },
+    },
   ],
 };
