e363f5454052093c82105f90e701edfb0ae9da47
Luca Lischetti <luca@sirlisko.com>
2019-03-08 12:00:13 +0000

    added metatags

--- web/src/components/MetaTags.jsx
+++ web/src/components/MetaTags.jsx
@@ -2,19 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-import metaTags from 'src/meta-tags';
-
 const MetaTags = ({ location: { pathname } = {} }) => {
-  let meta = metaTags.metaTags.find(mt => mt.url === pathname);
-  if (!meta) {
-    meta = metaTags.default;
-  }
   return (
-    <Helmet>
-      <title>{meta.title}</title>
-      <meta name="description" content={meta.description} />
-      <meta name="keywords" content={meta.keywords} />
-    </Helmet>
+    <Helmet
+      htmlAttributes={{
+        lang: 'en',
+      }}
+      title="Antenatal Classes in London | Artemis Midwifery"
+      meta={[
+        {
+          name: 'description',
+          content: 'Antenatal Classes in London',
+        },
+        {
+          property: 'og:title',
+          content: 'Artemis Midwifery',
+        },
+        {
+          property: 'og:image',
+          content: '/imgs/homepage.jpg',
+        },
+        {
+          property: 'og:description',
+          content: 'Antenatal Classes in London',
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          name: 'twitter:creator',
+          content: 'Artemis Midwifery',
+        },
+        {
+          name: 'twitter:title',
+          content: 'Artemis Midwifery',
+        },
+        {
+          name: 'twitter:description',
+          content: 'Antenatal Classes in London',
+        },
+        {
+          name: 'keywords',
+          content: 'midwife, antenatal, postnatal, classes, london',
+        },
+      ]}
+    />
   );
 };
 
